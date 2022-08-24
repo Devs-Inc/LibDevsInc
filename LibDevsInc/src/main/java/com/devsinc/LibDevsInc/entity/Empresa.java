04d33d95de159e701bd0595d006af9da1857f29b
Gabriel Enrique Romero Orozco <gabrielhro10@gmail.com>
2022-08-23 20:53:12 -0500

    se a actualizado el contenido de la clase Empresa, se agregaron atributos, costrutor y metodos

--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Empresa.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Empresa.java
@@ -1,4 +1,49 @@
 package com.devsinc.LibDevsInc.entity;
 
 public class Empresa {
+
+    private string nombre;
+    private string direccion;
+    private string telefono;
+    private string NIT;
+
+    public Empresa(string nombre, string direccion, string telefono, string NIT) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.NIT = NIT;
+    }
+
+    public string getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(string nombre) {
+        this.nombre = nombre;
+    }
+
+    public string getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(string direccion) {
+        this.direccion = direccion;
+    }
+
+    public string getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(string telefono) {
+        this.telefono = telefono;
+    }
+
+    public string getNIT() {
+        return NIT;
+    }
+
+    public void setNIT(string NIT) {
+        this.NIT = NIT;
+    }
+
 }
