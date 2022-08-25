71b4c4a98439dffd1dc1f1588d25cdf32146d0ae
Mauricio Corredor <hmcorredor@hotmail.com>
2022-08-24 21:51:17 -0500

    Creación clase Movimiento, autores: Cristian y Mauricio

--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Movimiento.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Movimiento.java
@@ -1,5 +1,39 @@
 package com.devsinc.LibDevsInc.entity;
 
 public class Movimiento {
-    private int prueba;
+    private double movimiento;
+    private String concepto;
+    private String usuarioRegistro;
+
+    //Constructor
+    public Movimiento(double movimiento, String concepto, String usuarioRegistro) {
+        this.movimiento = movimiento;
+        this.concepto = concepto;
+        this.usuarioRegistro = usuarioRegistro;
+    }
+
+    //Getters & Setters
+    public double getMovimiento() {
+        return movimiento;
+    }
+
+    public void setMovimiento(double movimiento) {
+        this.movimiento = movimiento;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getUsuarioRegistro() {
+        return usuarioRegistro;
+    }
+
+    public void setUsuarioRegistro(String usuarioRegistro) {
+        this.usuarioRegistro = usuarioRegistro;
+    }
 }
