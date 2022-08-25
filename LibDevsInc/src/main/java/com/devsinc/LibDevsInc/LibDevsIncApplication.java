8494362030a7fb1dd5bbafef02c9a85e67ed6261
Tulio-Rangel <tjrn20@hotmail.com>
2022-08-25 12:53:35 -0500

    Test de la clase empresa

--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/LibDevsIncApplication.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/LibDevsIncApplication.java
@@ -1,5 +1,6 @@
 package com.devsinc.LibDevsInc;
 
+import com.devsinc.LibDevsInc.entity.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibDevsIncApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibDevsIncApplication.class, args);
+		//SpringApplication.run(LibDevsIncApplication.class, args);
+
+		Empresa libreria = new Empresa("LibDev", "Calle falsa 123", "0000", "123");
+		System.out.println(libreria.getNombre());
+		libreria.setNombre("Panaderia");
+		System.out.println(libreria.getNombre());
 	}
 
 }
--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Empresa.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Empresa.java
@@ -2,47 +2,47 @@ package com.devsinc.LibDevsInc.entity;
 
 public class Empresa {
 
-    private string nombre;
-    private string direccion;
-    private string telefono;
-    private string NIT;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String NIT;
 
-    public Empresa(string nombre, string direccion, string telefono, string NIT) {
+    public Empresa(String nombre, String direccion, String telefono, String NIT) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
         this.NIT = NIT;
     }
 
-    public string getNombre() {
+    public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(string nombre) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public string getDireccion() {
+    public String getDireccion() {
         return direccion;
     }
 
-    public void setDireccion(string direccion) {
+    public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
-    public string getTelefono() {
+    public String getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(string telefono) {
+    public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    public string getNIT() {
+    public String getNIT() {
         return NIT;
     }
 
-    public void setNIT(string NIT) {
+    public void setNIT(String NIT) {
         this.NIT = NIT;
     }
 
