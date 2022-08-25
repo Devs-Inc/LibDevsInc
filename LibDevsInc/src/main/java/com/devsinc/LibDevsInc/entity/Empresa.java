190193e9c2de5441db5b1b9f12db0a5c6faae55c
Gabriel Romero <45883253+gabrielhro10@users.noreply.github.com>
2022-08-24 20:52:20 -0500

    Update Empresa.java
    se eliminaron errores del commit --amend

--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Empresa.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Empresa.java
@@ -2,28 +2,18 @@ package com.devsinc.LibDevsInc.entity;
 
 public class Empresa {
 
-<<<<<<< HEAD
-    private String nombre;
+private String nombre;
     private String direccion;
     private String telefono;
     private String NIT;
 
     public Empresa(String nombre, String direccion, String telefono, String NIT) {
-=======
-    private String nombre;
-    private String direccion;
-    private String telefono;
-    private String NIT;
-
-    public Empresa(String nombre, String direccion, String telefono, String NIT) {
->>>>>>> 04d33d95de159e701bd0595d006af9da1857f29b
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
         this.NIT = NIT;
     }
 
-<<<<<<< HEAD
     public String getNombre() {
         return nombre;
     }
@@ -55,38 +45,4 @@ public class Empresa {
     public void setNIT(String NIT) {
         this.NIT = NIT;
     }
-=======
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(string nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(string direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(string telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getNIT() {
-        return NIT;
-    }
-
-    public void setNIT(string NIT) {
-        this.NIT = NIT;
-    }
-
->>>>>>> 04d33d95de159e701bd0595d006af9da1857f29b
 }
