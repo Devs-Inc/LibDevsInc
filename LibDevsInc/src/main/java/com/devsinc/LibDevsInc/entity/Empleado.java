2ffe8bfb42fd853230d7e4fcdace33454808dffe
Maikinu <mlopezsi@hotmail.com>
2022-08-26 08:58:52 -0500

    Construcción clase empleado

--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Empleado.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/entity/Empleado.java
@@ -1,4 +1,48 @@
 package com.devsinc.LibDevsInc.entity;
 
 public class Empleado {
+
+    private Strindg nombreEmpleado;
+    private String correoEmpleado;
+    private Empresa empresaEmpleado;
+    private String rol;
+
+    public Empleado(Strindg nombreEmpleado, String correoEmpleado, Empresa empresaEmpleado, String rol) {
+        this.nombreEmpleado = nombreEmpleado;
+        this.correoEmpleado = correoEmpleado;
+        this.empresaEmpleado = empresaEmpleado;
+        this.rol = rol;
+    }
+
+    public Strindg getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(Strindg nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public Empresa getEmpresaEmpleado() {
+        return empresaEmpleado;
+    }
+
+    public void setEmpresaEmpleado(Empresa empresaEmpleado) {
+        this.empresaEmpleado = empresaEmpleado;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
