1bc4724cf76150f35d05262f5411412ff105da40
Maikinu <mlopezsi@hotmail.com>
2022-09-07 22:44:32 -0500

    Creacion repositorios

---
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/repositorios/repositorioMovimientosDinero.java
@@ -0,0 +1,7 @@
+package com.devsinc.LibDevsInc.repositorios;
+
+import com.devsinc.LibDevsInc.entity.*
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface repositorioMovimientosDinero extends JpaRepository<MovimientoDinero, Long>{
+}
