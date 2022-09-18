2e8ba3ee691ca4071a5ae9e27352a232a14c262d
Tulio-Rangel <tjrn20@hotmail.com>
2022-09-18 16:26:26 -0500

    sirve todo lo de empresa menos actualizar

--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/controller/ControlEmpresa.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/controller/ControlEmpresa.java
@@ -20,31 +20,31 @@ public class ControlEmpresa{
 
     @GetMapping("/enterprises")
     public List<Empresa> allEmpresas(){
-        return this.serviciosEmp.getListaEmpresas();
+        return this.serviciosEmp.allEmpresas();
     }
 
     //Consultar una empresa  de las disponibles
     @GetMapping("/enterprises/{id}")
     public Empresa consultarEmpresa(@PathVariable(value = "id") Long id){
-        return this.serviciosEmp.getListaEmpresa(id);
+        return this.serviciosEmp.findEmpresaById(id);
     }
 
     // Crear una empresa
     @PostMapping("/enterprises")
     public Empresa crearEmpresa(@RequestBody Empresa e){
-        return this.serviciosEmp.crearEmpresa(e);
+        return this.serviciosEmp.createEmpresa(e);
     }
 
     //editar una registro
-    @PatchMapping("/enterprises/{id}")
-    public Empresa patchEmpresa(@PathVariable Long id, @RequestBody Empresa e){
-        return this.serviciosEmp.patchEmp(id,e);
-    }
+//    @PatchMapping("/enterprises/{id}")
+//    public Empresa patchEmpresa(@PathVariable Long id, @RequestBody Empresa e){
+//        return this.serviciosEmp.updateEmpresa(id,e);
+//    }
 
     //borrar una registro
     @DeleteMapping("/enterprises/{id}")
-    public Empresa deleteEmpresa(@PathVariable(value = "id") Long id){
-        return this.serviciosEmp.deleteEmp(id);
+    public String deleteEmpresa(@PathVariable(value = "id") Long id){
+        return this.serviciosEmp.deleteEmpresa(id);
     }
 
 
--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/service/ServiciosEmpresa.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/service/ServiciosEmpresa.java
@@ -2,50 +2,34 @@ package com.devsinc.LibDevsInc.service;
 
 import com.devsinc.LibDevsInc.entity.Empresa;
 import com.devsinc.LibDevsInc.repository.RepositorioEmpresas;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ServiciosEmpresa {
+    @Autowired
+    private RepositorioEmpresas repositorioEmpresas;
 
-    //repEmp es un atributo de Empresa para diferenciarlo de Empleado.
-    private RepositorioEmpresas repEmp;
-
-    public ServiciosEmpresa(RepositorioEmpresas repEmp) {
-        this.repEmp = repEmp;
+    public Empresa createEmpresa(Empresa empresa) {
+        return repositorioEmpresas.save(empresa);
     }
 
-    //metodo GET para listar empresas
-    public List<Empresa> getListaEmpresas(){
-        return this.repEmp.findAll();
+    public List<Empresa> allEmpresas() {
+        return repositorioEmpresas.findAll();
     }
 
-    //metodo GET para consultar una empresa.
-    public Empresa getListaEmpresa(Long id){
-        return this.repEmp.findById(id).orElseThrow();
+    public Empresa findEmpresaById(Long id) {
+        return repositorioEmpresas.findById(id).get();
     }
 
-    // metodo POST para crear empresa.
-    public Empresa crearEmpresa(Empresa nuevoEmpresa){
-        return this.repEmp.save(nuevoEmpresa);
+    public Empresa updateEmpresa(Empresa empresa) {
+        return repositorioEmpresas.save(empresa);
     }
 
-    //Metodo PATCH para actualizar empresa.
-    public Empresa patchEmp(Long id, Empresa e){
-        Empresa empresaActual = repEmp.findById(id).orElseThrow();
-        empresaActual.setNombre(e.getNombre());
-        empresaActual.setDireccion(e.getDireccion());
-        empresaActual.setTelefono(e.getTelefono());
-        //empresaActual.setNIT(e.getNIT());
-        return this.repEmp.save(empresaActual);
+    public String deleteEmpresa(Long id) {
+        repositorioEmpresas.deleteById(id);
+        return "la empresa con ID " + id + " fue eliminada exitosamente";
     }
-
-    //Metodo Delete para eliminar empresa.
-    public Empresa deleteEmp(Long id) {
-        this.repEmp.deleteById(id);
-        // return  String "Registro Eliminado";
-        return null;
-    }
-
 }
