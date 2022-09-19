d659f15aae5239c0158c35e77ea964121f08a929
Tulio-Rangel <tjrn20@hotmail.com>
2022-09-19 16:24:12 -0500

    Control del back de empleados donde

--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/controller/ControlEmpleado.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/controller/ControlEmpleado.java
@@ -2,11 +2,53 @@ package com.devsinc.LibDevsInc.controller;
 
 import com.devsinc.LibDevsInc.entity.Empleado;
 import com.devsinc.LibDevsInc.service.ServiciosEmpleado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 public class ControlEmpleado {
+
+    @Autowired
+    ServiciosEmpleado serviciosEmpleado;
+
+    @GetMapping("/users")
+    public String listaUsers(Model model) {
+        model.addAttribute("user", serviciosEmpleado.allEmpleados());
+        return "users";
+    }
+
+    @GetMapping("/add-user")
+    public String addUser(Empleado empleado) {
+        return "add-user";
+    }
+
+    @PostMapping("/users")
+    public String createUser(Empleado empleado) {
+        serviciosEmpleado.createEmpleado(empleado);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users/borrar/{id}")
+    public String eliminarUser(@PathVariable("id") Long id) {
+        serviciosEmpleado.deleteEmpleado(id);
+        return "redirect/users";
+    }
+
+    @GetMapping("/users/editar/{id}")
+    public String findUserById(@PathVariable("id") Long id, Model model) {
+        Empleado empleado = serviciosEmpleado.findEmpleadoById(id);
+        model.addAttribute("user", empleado);
+        return "actualizar-user";
+    }
+
+    @PostMapping("/users/actualizar/{id}")
+    public String actualizarUser(@PathVariable("id") Long id, Empleado empleado) {
+        serviciosEmpleado.updateEmpleado(id, empleado);
+        return "redirect:/users";
+    }
 
 //    ServiciosEmpleado serviciosTra;
 //
