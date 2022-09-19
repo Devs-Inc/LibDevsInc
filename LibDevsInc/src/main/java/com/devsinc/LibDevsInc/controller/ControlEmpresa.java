ca4b2210cd44c91ea80e0c0fe7cdf2e0e4a5b5df
Tulio-Rangel <tjrn20@hotmail.com>
2022-09-19 10:45:06 -0500

    sirven los controladores de empresa

--- LibDevsInc/src/main/java/com/devsinc/LibDevsInc/controller/ControlEmpresa.java
+++ LibDevsInc/src/main/java/com/devsinc/LibDevsInc/controller/ControlEmpresa.java
@@ -2,49 +2,45 @@ package com.devsinc.LibDevsInc.controller;
 
 import com.devsinc.LibDevsInc.entity.Empresa;
 import com.devsinc.LibDevsInc.service.ServiciosEmpresa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-public class ControlEmpresa{
+public class ControlEmpresa {
 
+    @Autowired
     ServiciosEmpresa serviciosEmp;
 
-    //constructor
-    public ControlEmpresa(ServiciosEmpresa serviciosEmp){
-        this.serviciosEmp = serviciosEmp;
-    }
-
-    // Consultar todas las empresas disponibles
-
+    //Consultar todas las empresas disponibles
     @GetMapping("/enterprises")
     public List<Empresa> allEmpresas(){
-        return this.serviciosEmp.allEmpresas();
+        return serviciosEmp.allEmpresas();
     }
 
     //Consultar una empresa  de las disponibles
     @GetMapping("/enterprises/{id}")
     public Empresa consultarEmpresa(@PathVariable(value = "id") Long id){
-        return this.serviciosEmp.findEmpresaById(id);
+        return serviciosEmp.findEmpresaById(id);
     }
 
     // Crear una empresa
     @PostMapping("/enterprises")
     public Empresa crearEmpresa(@RequestBody Empresa e){
-        return this.serviciosEmp.createEmpresa(e);
+        return serviciosEmp.createEmpresa(e);
     }
 
-    //editar una registro
+    //editar una empresa
     @PatchMapping("/enterprises/{id}")
     public Empresa patchEmpresa(@PathVariable Long id, @RequestBody Empresa e){
-        return this.serviciosEmp.updateEmpresa(id,e);
+        return serviciosEmp.updateEmpresa(id,e);
     }
 
-    //borrar una registro
+    //borrar una empresa
     @DeleteMapping("/enterprises/{id}")
     public String deleteEmpresa(@PathVariable(value = "id") Long id){
-        return this.serviciosEmp.deleteEmpresa(id);
+        return serviciosEmp.deleteEmpresa(id);
     }
 
 
